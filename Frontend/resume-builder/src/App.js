91aec16913b0b92a54d0f44aeb8a8761c0042a09
Ashish Kumar Mishra <ashishmishra19122000@gmail.com>
2023-06-15 02:52:45 +0530

    Frontend

---
+++ Frontend/resume-builder/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ResumeSectionView from "./components/ResumeSectionView";
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeSectionView />
+    </div>
+  );
+}
+
+export default App;
